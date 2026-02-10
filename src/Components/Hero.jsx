97778680809ffb53d4eb34a7e1ec7beb26a8323b
andi <andimfauzan2008@gmail.com>
2026-02-10 08:44:31 +0700

    fixed img path

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Samyang from "../assets/samyang.jpg";
+import Samyang from "../assets/Samyang.jpg";
 
 const Hero = () => {
   return (
@@ -7,12 +7,12 @@ const Hero = () => {
       <div className="ml-12">
         <h1 className="font-bold text-6xl">Samyang Rolls</h1>
         <h2 className="font-semibold text-4xl my-4">Pedas & Gurih!</h2>
-        <button className="border-[2px] rounded-2xl border-gray-300 shadow-black p-3 bg-white font-semibold text-xl justify-center hover:translate-y-[-2px]">
+        <button className="border-[2px] rounded-2xl border-gray-300 shadow-black p-3 bg-white font-semibold text-xl justify-center hover:translate-y-[-2px] mt-8" >
           Order Sekarang!
         </button>
       </div>
       <div className="mr-12">
-        <img src={Samyang} alt="Samyang Image" className="border-4 border-gray- rounded-[120px]"/>
+        <img src={Samyang} alt="Samyang Image" className="border-4 rounded-[120px]"/>
       </div>
     </div>
   );
