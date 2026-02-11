fdf8e61b2d34b20927fef33b7114750c325f6e71
andi <andimfauzan2008@gmail.com>
2026-02-11 19:12:48 +0700

    added Keungulan section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import Deskripsi from './Components/Deskripsi'
 import ProblemSolution from './Components/ProblemSolution'
+import Features from './Components/Features'
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
     <Navbar/>
     <Hero/>
     <ProblemSolution/>
-    <Deskripsi/>
+    <Features/>
     </>
   )
 }
