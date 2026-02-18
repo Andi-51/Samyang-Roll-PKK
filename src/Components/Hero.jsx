b116227accd535059e342d56c629c99aec2483ba
andi <andimfauzan2008@gmail.com>
2026-02-18 07:33:25 +0700

    small animation fixes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,17 +8,17 @@ const Hero = () => {
       <div className="mx-auto max-w-[1240px] w-full px-6 grid grid-cols-1 md:grid-cols-2 items-center gap-10">
         {/* TEXT */}
         <div className="text-center md:text-left">
-          <motion.div initial={{ opacity:0, y:-80 }} animate={{ opacity:1, y:0 }} viewport={{ once:true }} transition={{ duration:0.8, delay:0.2 }}>
+          <motion.div initial={{ opacity:0, y:-80 }} animate={{ opacity:1, y:0 }} viewport={{ once:true }} transition={{ duration:0.4, delay:0.2 }}>
             <h1 className="font-bold text-4xl lg:text-6xl">Samyang Rolls</h1>
           </motion.div>
-          <motion.div initial={{ opacity:0, x:-80 }} animate={{ opacity:1, x:0 }} viewport={{ once:true }} transition={{ duration:0.8, delay:0.4 }}>
+          <motion.div initial={{ opacity:0, x:-80 }} animate={{ opacity:1, x:0 }} viewport={{ once:true }} transition={{ duration:0.4, delay:0.25 }}>
             <h2 className="font-semibold text-2xl lg:text-4xl my-4">
               Pedas & Gurih!
             </h2>
           </motion.div>
-          <div className="mt-8">
-          <motion.div initial={{ opacity:0, y:80 }} animate={{ opacity:1, y:0 }} viewport={{ once:true }} transition={{ duration:0.8, delay:0.5 }}>
-            <a href="#order" className="bg-white rounded-full px-6 py-3 mt-4 font-semibold text-lg hover:-translate-y-1 transition text-orange-500">
+          <div className="mt-8 w-fit">
+          <motion.div initial={{ opacity:0, y:80 }} animate={{ opacity:1, y:0 }} viewport={{ once:true }} transition={{ duration:0.4, delay:0.3 }} whileHover={{ y:-4, transition:{ duration:0.1, delay:0 } }} className="w-fit">
+            <a href="#order" className="bg-white rounded-full px-6 py-3 mt-4 font-semibold text-lg hover:-translate-y-1 transition text-orange-500 w-fit">
               Order Sekarang!
             </a>
           </motion.div>
@@ -27,7 +27,7 @@ const Hero = () => {
 
         {/* IMAGE */}
         <div className="flex justify-center md:justify-end">
-          <motion.div initial={{ opacity:0, x:80 }} animate={{ opacity:1, x:0 }} viewport={{ once:true }} transition={{ duration:0.8, delay:0.6 }} className="overflow-x-hidden">
+          <motion.div initial={{ opacity:0, x:80 }} animate={{ opacity:1, x:0 }} viewport={{ once:true }} transition={{ duration:0.4, delay:0.6 }} className="overflow-x-hidden">
           <img
             src={Samyang3}
             alt="Samyang Image"
