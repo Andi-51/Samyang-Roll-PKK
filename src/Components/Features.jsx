3d1bc75b202fb412200aef63a7318933995c3821
andi <andimfauzan2008@gmail.com>
2026-02-18 07:17:57 +0700

    small animation fixes

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -30,12 +30,12 @@ const Features = () => {
                 <h1 className="font-bold text-xl my-2">Pedas yang Autentik</h1>
                 <p className="text-center text-gray-700">Rasa pedas yang bikin candu hingga menggoyangkan lidah</p>
             </div>
-            <div data-aos="flip-right" data-aos-duration="500" data-aos-delay="400"  className="bg-white p-4 rounded-xl shadow-lg flex flex-col items-center">
+            <div data-aos="flip-right" data-aos-duration="500" data-aos-delay="350"  className="bg-white p-4 rounded-xl shadow-lg flex flex-col items-center">
                 <Utensils size={48} className="text-amber-500 mb-2"/>
                 <h1 className="font-bold text-xl my-2">Unik & Modern</h1>
                 <p className="text-center text-gray-700">Mie yang digulung menggunakan rice paper, menghasilkan texture yang kenyal dan berbeda dari yang lain</p>
             </div>
-            <div data-aos="flip-right" data-aos-duration="500" data-aos-delay="600"  className="bg-white p-4 rounded-xl shadow-lg flex flex-col items-center">
+            <div data-aos="flip-right" data-aos-duration="500" data-aos-delay="550"  className="bg-white p-4 rounded-xl shadow-lg flex flex-col items-center">
                 <Clock size={48} className="text-amber-500 mb-2"/>
                 <h1 className="font-bold text-xl my-2">Praktis & Cepat</h1>
                 <p className="text-center text-gray-700">Siap dinikmati dimana pun tanpa ribet, cocok untuk cemilan segala waktu</p>
